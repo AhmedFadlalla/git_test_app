6edbda2413d1786b52b95aa21a95ce4f5ba5141c
Ahmed Fadlalla <88683309+AhmedFadlalla@users.noreply.github.com>
2023-09-14 21:31:33 +0300

    Update test_screen.dart

--- lib/test_screen.dart
+++ lib/test_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class TestScreen extends StatelessWidget {
   const TestScreen({Key? key}) : super(key: key);
-
+//git edit
   @override
   Widget build(BuildContext context) {
     return Scaffold();
